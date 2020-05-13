1f192c31bdc685d5022145a2711acbdc8d3d57b7
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-05-14 00:17:32 +0530

    v1

---
+++ client/build/precache-manifest.df583b3dcaea863f41905c3298ffb387.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccb078911496af2208c3c5ce8a35a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fa21a1139c59824816",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "98652ca2c47e448ccc6f",
+    "url": "/static/css/main.3f415067.chunk.css"
+  },
+  {
+    "revision": "89fa21a1139c59824816",
+    "url": "/static/js/2.4473d917.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4473d917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98652ca2c47e448ccc6f",
+    "url": "/static/js/main.4e65b250.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "d350ce249a30596db0ed98838873a907",
+    "url": "/static/media/Comfortaa-Regular.d350ce24.ttf"
+  },
+  {
+    "revision": "fb2192a677f1db6030f0ed2d1ba2f0d8",
+    "url": "/static/media/trackIcon.fb2192a6.png"
+  }
+]);
